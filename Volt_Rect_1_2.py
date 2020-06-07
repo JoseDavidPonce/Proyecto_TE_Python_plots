5c3dce2beb03411d0ca44c81634a56d485d7b44e
fer18171 <66540300+fer18171@users.noreply.github.com>
2020-06-06 23:31:44 -0600

    Add files via upload

---
+++ Volt_Rect_1_2.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import sympy as sy
+from sympy.abc import x,y
+from numpy import exp,sinh,sin,cos,tan,arctan,pi
+
+
+
+
+fig=plt.figure()
+ax=Axes3D(fig)
+a=3
+b=1.25
+N=20
+
+
+x=np.linspace(-b,a,1000)
+y=np.linspace(0,b,1000)
+X, Y=np.meshgrid(x,y)
+
+
+def Ponce(n,b):
+    return sy.integrate((2*y**3+5)*sy.sin(n*pi*y/b),(y,0,b))
+
+def An(n,a,b):
+    return ((exp(n*pi)-exp(-n*pi*a/b))/(b*sinh(n*pi*(a/b+1))))*Ponce(n,b)
+
+def Bn(n,a,b):
+    return ((exp(n*pi)-exp(-n*pi*a/b))/(b*sinh(n*pi*(a/b+1))))*Ponce(n,b)
+
+def z(x,y):
+    Z=0
+    for k in range(N):
+        n=k+1
+        Z=Z+(An(n,a,b)*exp(n*pi*x/b)+Bn(n,a,b)*exp(-n*pi*x/b))*sin(n*pi*y/b)
+    return Z
+
+
+ax.plot_surface(X,Y,z(X,Y))
+plt.title("Voltaje n="+str(n))
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
+
