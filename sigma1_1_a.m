22894554fd940b557eb7592873b2d9140eda1de3
JoseDavidPonce <60592092+JoseDavidPonce@users.noreply.github.com>
2020-06-08 00:32:01 -0600

    Paredes a y b problema 1.1

--- sigma1_1_a.m
+++ sigma1_1_a.m
@@ -1,13 +1,17 @@
 a=2;
 b=3;
-n=20;
+i=20;
 
 y=linspace(0,3,200);
 e=8.85.*10.^(-12);
+sigmatotal = 0;
+for n = 1:i
+  An = 1./(n.*pi.*sinh(n.*pi.*a./b)).*(cos(n.*pi).*((2.*b.^3)./(a.^3.*(n.*pi).^2)-atan(b./a)));
+  sigma = -2.*pi.*e.*n./b.*An.*cosh(n.*pi.*a./b).*sin(n.*pi.*y./b);
+  sigmatotal = sigma+sigmatotal;
+endfor
 
-An = 1./(n.*pi.*sinh(n.*pi.*a./b)).*(cos(n.*pi).*((2.*b.^3)./(a.^3.*(n.*pi).^2)-atan(b./a)))
-sigma = -2.*pi.*e.*n./b.*An.*cosh(n.*pi.*a./b).*sin(n.*pi.*y./b)
-
-plot(y,sigma)
+title("Pared x=a")
+plot(y,sigmatotal)
 xlabel("y")
 ylabel("Densidad de carga")
--- sigma1_1_b_pared_xa.m
+++ sigma1_1_b_pared_xa.m
@@ -1,6 +1,6 @@
 a=2;
 b=3;
-i=15;
+i=20;
 
 y=linspace(0,3,300);
 e=8.85.*10.^(-12);
@@ -10,7 +10,7 @@ for n = 1:i
   integral = cos(n.*pi).*((2.*b.^3+5)./(n.*pi)+12.*b.^3./(n.*pi).^3)+5./(n.*pi);
   An = (exp(n.*pi)-exp(n.*pi.*a./b))./(b.*sinh(n.*pi.*(a./b+1))).*integral;
   Bn = (exp(n.*pi.*a./b)-exp(-n.*pi))./(b.*sinh(n.*pi.*(a./b+1))).*integral;
-  sigma = e.*pi./b.*n.*(An.*exp(-n.*pi)-Bn.*exp(n.*pi)).*sin(n.*pi.*y./b);
+  sigma = -e.*pi./b.*n.*(An.*exp(n.*pi.*a./b)-Bn.*exp(-n.*pi.*a./b)).*sin(n.*pi.*y./b);
   sigmatotal= sigmatotal+sigma;
 endfor
 plot(y,sigmatotal)
