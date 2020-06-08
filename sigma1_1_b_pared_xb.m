2c8d36bcb0a4fd36359506aaa67defe936606a44
JoseDavidPonce <60592092+JoseDavidPonce@users.noreply.github.com>
2020-06-07 23:53:07 -0600

    Integral pared 1.1b

--- sigma1_1_b_pared_xb.m
+++ sigma1_1_b_pared_xb.m
@@ -5,8 +5,11 @@ n=20;
 y=linspace(0,3,200);
 e=8.85.*10.^(-12);
 
+integral = cos(n.*pi).*((2.*b.^3+5)./(n.*pi)+12.*b.^3./(n.*pi).^3)+5./(n.*pi)
+
+
 An = 1./(n.*pi.*sinh(n.*pi.*a./b)).*(cos(n.*pi).*((2.*b.^3)./(a.^3.*(n.*pi).^2)-atan(b./a)))
-sigma = -2.*pi.*e.*n./b.*An.*cosh(n.*pi.*a./b).*sin(n.*pi.*y./b)
+sigma = e.*pi./b.*n.*(An.*exp(-n.*pi)-Bn.*exp(n.*pi)).*sin(n.*pi.*y./b)
 
 plot(y,sigma)
 xlabel("y")
