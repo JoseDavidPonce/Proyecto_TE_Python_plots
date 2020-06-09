d3a59dacf64e93e033f1c2c10f1e0d4b551e8e36
diegomencos <66656710+diegomencos@users.noreply.github.com>
2020-06-08 20:50:30 -0600

    Problema Esfericas Corregido

---
+++ Prob_Esf_2.m
@@ -0,0 +1,15 @@
+a=2;
+b=3;
+fi=-5;
+k=2;
+r1=linspace(0,7.*b,100);
+z1=linspace(0,pi,100);
+e=1;
+[r,z]=meshgrid(r1,z1);
+
+V=(fi).*(r<a)+(((-b.*k./e).*(1-a./r))+((2.*k)./(15.*b.*e)).*(r.^2-(a.^5./r.^3)).*(3.*(cos(z).^2)-1)+(fi.*a./r)).*(r>=a & r<=b)+(((-b.*k.*(b-a))./(e.*r))+((2.*k.*(b.^5-a.^5))./(15.*b.*e.*r.^3)).*(3.*cos(z).^2-1)+(fi.*a./r)).*(r>b);
+
+surf(r,z,V)
+xlabel("r")
+ylabel("Theta")
+zlabel("Voltaje")
